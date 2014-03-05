14a49875ed8b613806bd806f7bda965019c56416
linminglu <lingminglu@163.com>
2014-03-05 14:06:06 +0800

    init ios project 初始化项目

---
+++ AppCard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppCard
+//
+//  Created by linminglu on 14-3-5.
+//  Copyright (c) 2014年 linminglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ AppCardTests/AppCardTests.m
@@ -0,0 +1,34 @@
+//
+//  AppCardTests.m
+//  AppCardTests
+//
+//  Created by linminglu on 14-3-5.
+//  Copyright (c) 2014年 linminglu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppCardTests : XCTestCase
+
+@end
+
+@implementation AppCardTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
